eec38f1440e879be1baa4b8139db95c07fab11ee
Alvin <hello@alvin.codes>
2022-07-20 16:12:47 +0100

    Finessing ✨

--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,6 @@
 const Content = () => {
   return (
     <>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <p>
-        This file uses values sourced from the{' '}
-        <a href="https://f36.contentful.com/">Forma36 tokens</a>.
-      </p>
-      <p>
-        This a <button>Button</button>
-      </p>
-      <p>
-        <button className="button-large">Large button</button>
-
-        <button className="button-large button-secondary">
-          Large secondary button
-        </button>
-      </p>
       <section className="river">
         <img
           src="https://images.ctfassets.net/p39nycxzit31/U3SK6xjRhpQRa9vJy8nyM/b57a359f6fed8bf3e3b8cc7af66bb896/Hero.png?w=2368&h=1776&q=50&fm=webp"
@@ -51,7 +32,10 @@ const Content = () => {
       <section className="river">
         <article>
           <h2>Get started using React</h2>
-          <p>Use the checkboxes below to view paintings based on their tags.</p>
+          <p>
+            Use the checkboxes below to filter paintings based on their tags in
+            Contentful.
+          </p>
 
           <ul>
             <li>Fetches data from the Contentful REST API</li>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './starter.css'
 import './index.css'
 import App from './App'
 
